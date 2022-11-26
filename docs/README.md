b16febe27b71bb9bf99c18df69aabf7950107a9b
HaODay <ycis5013@naver.com>
2022-11-26 14:04:19 +0900

    feat(InputView): 게임 종료 후 재시작/종료 여부를 입력 받는 기능 구현

--- docs/README.md
+++ docs/README.md
@@ -20,8 +20,8 @@
         - [x] 일치하는 숫자가 다른 자리에 있으면 볼 출력
         - [x] 일치하는 숫자가 같은 자리에 있으면 스트라이크 출력
 
-- [ ] 게임이 끝난 경우 재시작/종료 여부를 입력 받는다. - InputView#readCommand
+- [x] 게임이 끝난 경우 재시작/종료 여부를 입력 받는다. - InputView#readCommand
     - 잘못된 값을 입력할 경우 IllegalArgumentException 예외 발생 - ValidateNumber#validateCommand
         - [ ] 1 또는 2가 아닌 경우
-    - [ ] 1을 입력 받으면 재시작한다.
-    - [ ] 2를 입력 받으면 종료한다.
+    - [x] 1을 입력 받으면 재시작한다.
+    - [x] 2를 입력 받으면 종료한다.
--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -6,8 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputView {
-    public static void readPlayerNum(){
+    public static List<Integer> readPlayerNum(){
         System.out.print("숫자를 입력해주세요 : ");
-        ValidateInput.validatePlayerNum(Console.readLine());
+        return ValidateInput.validatePlayerNum(Console.readLine());
+    }
+
+    public static void readCommand(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if(ValidateInput.validateCommand(Console.readLine())==1){
+            Application.playGame();
+        }
     }
 }
